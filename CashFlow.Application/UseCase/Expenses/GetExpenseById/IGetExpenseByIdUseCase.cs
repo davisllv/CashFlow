ccb1dafacabac45b93cd391ace5ae7cb38053c89
Davi da Silva dos Santos <davi.santos@scoreplan.com.br>
2024-12-23 15:38:39 -0300

    Verified Commit

--- CashFlow.Application/UseCase/Expenses/GetExpenseById/IGetExpenseByIdUseCase.cs
+++ CashFlow.Application/UseCase/Expenses/GetExpenseById/IGetExpenseByIdUseCase.cs
@@ -3,5 +3,6 @@
 namespace CashFlow.Application.UseCase.Expenses.GetExpenseById;
 public interface IGetExpenseByIdUseCase
 {
+    // --- Verified Commit
     public Task<ResponseExpenseJson> Execute(long id);
 }
