69b1d9acfa199cb19325b53c29d6004b6a935d48
Davi da Silva <davisllvsnt57@gmail.com>
2025-01-17 14:28:13 -0300

    Initiate the useCase test

---
+++ test/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Application.UseCase.Users.Register;
+using CommomTestUtilities.Request;
+using FluentAssertions;
+
+namespace UseCases.Test.Users.Register;
+public class RegisterUserUseCaseTest
+{
+    [Fact]
+    public async Task Execute()
+    {
+        var useCase = CreateUseCase();
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var result = await useCase.Execute(request);
+
+        result.Should().NotBeNull();
+        result.Name.Should().Be(request.Name);
+        result.Token.Should().NotBeNullOrWhiteSpace();
+    }
+
+    private RegisterUserUseCase CreateUseCase()
+    {
+        return new RegisterUserUseCase();
+    }
+}
