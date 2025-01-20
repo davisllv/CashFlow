dbb8a51c7b059ce601bba1906eeecda6a9bef751
Davi da Silva <davisllvsnt57@gmail.com>
2025-01-20 11:19:47 -0300

    Configuring a memory database with entity.framework in memory

--- WebApi.Test/CustomWebApplicationFactory.cs
+++ WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using CashFlow.Infrastructure.DataAccess;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApi.Test;
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.UseEnvironment("Test");
+        // Preciso deixar o CashFlowDbContext Public para ter acesso aqui.
+        builder.UseEnvironment("Test")
+            .ConfigureServices(services =>
+            {
+                var serviceProvider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+                services.AddDbContext<CashFlowDbContext>(conf =>
+                {
+                    conf.UseInMemoryDatabase("InMemoryDbForTesting");
+                    conf.UseInternalServiceProvider(serviceProvider);
+                });
+
+            });
     }
 }
--- src/CashFlow.Infraestructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infraestructure/DependencyInjectionExtension.cs
@@ -49,7 +49,7 @@ public static class DependencyInjectionExtension
     private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Connection");
-        var serverVersion = new MySqlServerVersion(new Version(8, 0, 40));
+        var serverVersion = ServerVersion.AutoDetect(connectionString);
 
         services.AddDbContext<CashFlowDbContext>(config => config.UseMySql(connectionString, serverVersion));
     }
