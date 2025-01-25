7b22af1dd4ccee55fa7a0fe49611919746963831
Davi da Silva <davisllvsnt57@gmail.com>
2025-01-25 07:48:53 -0300

    Createda way to ignore de dbset and set a new name inside de tables in migrations

--- src/CashFlow.Infraestructure/DataAcess/CashFlowDbContext.cs
+++ src/CashFlow.Infraestructure/DataAcess/CashFlowDbContext.cs
@@ -7,6 +7,13 @@ public class CashFlowDbContext : DbContext
 {
     public CashFlowDbContext(DbContextOptions options) : base(options) { }
     // Eu passo para o construtor da classe base.
+    // Daqui que a Migration tira para criar as tabelas. Eu posso também, colocar o db set dentro das tabelas aqui dentro, isto é, dentro de expense, eu consigo dar um dbset e ai já basta.
     public DbSet<Expense> Expenses { get; set; }
     public DbSet<User> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Forma de remodelar as tabelas pelas migrations.
+        modelBuilder.Entity<Tag>().ToTable("Tags");
+    }
 }
--- src/CashFlow.Infraestructure/Migrations/20250125104445_TagsToExpense.Designer.cs
+++ src/CashFlow.Infraestructure/Migrations/20250125104445_TagsToExpense.Designer.cs
@@ -4,6 +4,7 @@ using CashFlow.Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CashFlow.Infraestructure.Migrations
 {
     [DbContext(typeof(CashFlowDbContext))]
-    partial class CashFlowDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250125104445_TagsToExpense")]
+    partial class TagsToExpense
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
